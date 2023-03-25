7cb714d106b7245cfd3382b3868a69d4b19ff0f3
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-03-26 01:19:27 +0300

    add home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Navbar from './components/NavBar/Navbar';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <Navbar/>
+      <>
+      <Navbar/>
+      <Home/>
+      </>
   );
 }
 
---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Home.css'
+
+export default function Home() {
+  return (
+    <div className = "hero">
+    <div class="card bg-dark text-white border-0">
+    <img src="/assets/bg.jpg" class="card-img" alt="Backgroumd height= 550px" />
+    <div class="card-img-overlay d-flex flex-column justify-content-center">
+        <div class="container">
+            <h5 class="card-title display-3 fw-bolder mb-0 text-info">NEW SEASON ARRIVALS</h5>
+            <p class="card-text lead fs-2 text-info">CHECK OUT ALL THE TRANDS</p>
+        </div>
+    </div>
+    </div>
+    
+    </div>
+  )
+}
