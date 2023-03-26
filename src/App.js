d8f9af66b93e2d6e26f1926937b0fb15eef27291
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-03-26 16:46:50 +0300

    Add thier routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Products from './components/Products/Products';
 import Product from './components/Product/Product';
 import {Routes, Route} from 'react-router-dom'
 import Cart from './components/Cart/Cart';
+import Login from './components/Login/Login';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
   return (
@@ -15,6 +17,10 @@ function App() {
               <Route path="/products" element={<Products />} />
               <Route path="/products/:id" element={<Product />} />
               <Route path="/cart" element={<Cart />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<SignUp />} />
+
+
 
 
           </Routes>
