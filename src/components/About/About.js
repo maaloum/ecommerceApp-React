e7ea458021dff68c4d8a8c0c9e4f1f77dc83e662
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-03-26 18:48:53 +0300

    create about

---
+++ src/components/About/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function About () {
+  return (
+    <section class="container my-5 py-5">
+    <div class="row">
+      <div class="col-md-6 mt-4 px-5">
+        <img src="./assets/screenshoot.png" alt="About Us Image" class="img-fluid"/>
+      </div>
+      <div class="col-md-6">
+        <h2>About Us</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel metus euismod, venenatis libero non, hendrerit velit. Donec varius lectus in convallis egestas. Sed ac tellus est. Fusce quis commodo massa, et pretium quam. Sed commodo, augue a gravida ultricies, erat enim ornare magna, at pretium nibh nibh sit amet nibh.</p>
+      </div>
+    </div>
+  </section>
+  )
+}
